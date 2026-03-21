959af93825114f226ee5c8d679dd2c54dd2832c1
Patrick Veverka <veverkap@users.noreply.github.com>
2026-03-20 21:55:00 -0400

    fix errors

--- internal/organize/rename_noreplace_linux.go
+++ internal/organize/rename_noreplace_linux.go
@@ -3,16 +3,40 @@
 package organize
 
 import (
-	"errors"
 	"os"
 	"syscall"
+	"unsafe"
+)
+
+const (
+	atFDCWD             = -100
+	renameNoReplaceFlag = 1
 )
 
 func renameNoReplace(oldPath, newPath string) error {
-	err := syscall.Renameat2(syscall.AT_FDCWD, oldPath, syscall.AT_FDCWD, newPath, syscall.RENAME_NOREPLACE)
-	if errors.Is(err, syscall.EEXIST) {
-		return os.ErrExist
+	oldPtr, err := syscall.BytePtrFromString(oldPath)
+	if err != nil {
+		return err
+	}
+	newPtr, err := syscall.BytePtrFromString(newPath)
+	if err != nil {
+		return err
 	}
 
-	return err
+	_, _, errno := syscall.Syscall6(
+		syscall.SYS_RENAMEAT2,
+		uintptr(atFDCWD),
+		uintptr(unsafe.Pointer(oldPtr)),
+		uintptr(atFDCWD),
+		uintptr(unsafe.Pointer(newPtr)),
+		uintptr(renameNoReplaceFlag),
+		0,
+	)
+	if errno == 0 {
+		return nil
+	}
+	if errno == syscall.EEXIST {
+		return os.ErrExist
+	}
+	return errno
 }
